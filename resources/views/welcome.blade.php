4fa3ec65d2ac3eb1c3a8942c525b0385fedd71d6
ch <mkmsacc@outlook.com>
2020-08-21 16:00:18 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS for Backend Devs</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+
+            <div class="header-top">
+                <h1>XCasts</h1>
+
+                <a href="#">Sign In</a>
+            </div>
+
+            <nav>
+                <a href="#">Catalog</a>
+                <a href="#">Series</a>
+                <a href="#">Podcast</a>
+                <a href="#">Disscussions</a>
+            </nav>
+        </div>
+
+
+
+    </header>
+
+</body>
+
+</html>
